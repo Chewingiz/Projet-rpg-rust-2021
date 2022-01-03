16313eb5ddc17590e843b47dceecf4955b605a09
Elsegahy <relsegahy@gmail.com>
2022-01-03 22:03:52 +0100

    ajout de commentaire sur les fonctions

--- projet_rust/src/main.rs
+++ projet_rust/src/main.rs
@@ -19,7 +19,7 @@ use termion::{
     raw::IntoRawMode,
     clear,
 };
-struct Perso{
+struct Perso{ // la structure de votre héro chaque paramètre influencera les actions possibles
     nom: String,
     charisme: u64,
     force: u64,
@@ -27,7 +27,7 @@ struct Perso{
 }
 
 fn main() -> Result<(), io::Error> {
-    let perso = Perso{
+    let perso = Perso{ // la structure de héro elle est pour l'instant static 
         nom: "Pachat".to_string(),
         charisme: 1,
         force: 2,
@@ -36,23 +36,21 @@ fn main() -> Result<(), io::Error> {
     let mut stdout = io::stdout().into_raw_mode()?;
     write!(stdout, "{}", clear::All)?;
     let backend = TermionBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
+    let mut terminal = Terminal::new(backend)?; // on crée donc ce qui nous sera l'interface ou l'utilisateur pourra interagir dans le terminal
     terminal.draw(|mut f| {
         let test = BarChart::default()
-        .block(Block::default().title("BarChart").borders(Borders::ALL))
-        .bar_width(1)
-        .bar_gap(5)
+        .block(Block::default().title("BarChart").borders(Borders::ALL)) // on précise que tous sera entouré par une bordure 
+        .bar_width(1) // d'une épaiseur de 1
+        .bar_gap(5) // enfin elle seront toutes une distances des uns des autres de 5 (les diférentes case)
         .style(Style::default().fg(Color::Yellow).bg(Color::Red))
         .value_style(Style::default().fg(Color::Red).modifier(Modifier::BOLD))
-        .label_style(Style::default().fg(Color::White))
-        .data(&[("Charisme", 2), ("Force", 4), ("Intelligence", 3)])
-        .max(3);
-        let chunks = Layout::default()
+        .label_style(Style::default().fg(Color::White)); // la couleur de la bordure sera blanche 
+        let chunks = Layout::default()// on crée les différents case de notre interface sur le terminal 
             .direction(Direction::Horizontal)
             .margin(1)
-            .constraints(
+            .constraints( //les differrents ecart entre chaque cases qu'on créera
                 [
-                    Constraint::Percentage(30),
+                    Constraint::Percentage(30), 
                     Constraint::Percentage(50),
                 ]
                 .as_ref(),
@@ -61,16 +59,16 @@ fn main() -> Result<(), io::Error> {
         let left_chunks = Layout::default()
             .direction(Direction::Vertical)
             .margin(1)
-            .constraints(
+            .constraints(//les differrents ecart entre chaque cases qu'on créera
                 [
-                    Constraint::Percentage(30),
+                    Constraint::Percentage(50),
                     Constraint::Percentage(50),
                 ]
                 .as_ref(),
             )
             .split(chunks[0]);
         
-            let option = [
+            let option = [ // l'affichage des différent static que vous possédés
                 Text::raw("q: pour quitter\ns: pour sauvegarder"),
                 Text::styled("", Style::default())
             ];
@@ -87,7 +85,7 @@ fn main() -> Result<(), io::Error> {
             Block::default().title("Histroire").borders(Borders::ALL);
          */   
                     
-        let text = [
+        let text = [ // le texte qui sera vue dans l'interface du terminal
    		 Text::raw("First line\n"),
     		Text::styled("C’est le matin, de la lumière orangée passe à travers vos rideaux de toile fine et brunie par le temps. Ils sont si anciens que vous ne vous souvenez plus de leur couleur originale… gris ? Marron peut-être… De toute façon vous n’en avez que faire, c’est la dernière fois que vous aurez l'occasion de les voir. Aujourd’hui vous partez.
 « Cette vie n’est pas faite pour moi» vous vous dites en regardant la tâche de moisie au-dessus de votre lit qui ressemble vaguement à un visage. Vous préparez rapidement vos affaires et ramassez quelque provision de celle qui ne semble pas encore létale (un morceau de pain sec) et une dague à la lame bleutée .
@@ -99,11 +97,11 @@ Vous êtes face à votre demeure… Enfin... vôtre cabane, l'aventure commence 
 \n", Style::default())
 	];
 let right_pane =Paragraph::new(text.iter())
-    .block(Block::default().title("Histroire").borders(Borders::ALL)).scroll(0)
+    .block(Block::default().title("Histroire").borders(Borders::ALL)).scroll(0)// on donne un titer a la case et on instancie le scroll(a modifier)
     .wrap(true);
         
         
-        f.render_widget(upper_left_pane, left_chunks[0]);
+        f.render_widget(upper_left_pane, left_chunks[0]);// on rajoute les differentes cases a notre interfaces
         f.render_widget(lower_left_pane, left_chunks[1]);
         f.render_widget(right_pane, chunks[1]);
     })?;
